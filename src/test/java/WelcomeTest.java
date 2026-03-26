eafb19a137939b502ff2bb319827d5a8226c589b
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-03-26 14:04:03 +0100

    "Class test"

---
+++ src/test/java/WelcomeTest.java
@@ -0,0 +1,34 @@
+import org.example.Welcome;
+import org.example.FakeClock;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WelcomeTest {
+
+    @Test
+    void shouldReturnBonjourInMorning() {
+
+        FakeClock clock = new FakeClock(LocalDateTime.of(2024, 3, 20, 10, 0));
+
+        Welcome welcome = new Welcome(clock);
+
+        String message = welcome.getMessage();
+
+        assertEquals("Bonjour", message);
+    }
+
+    @Test
+    void shouldReturnWeekendMessageOnSunday() {
+
+        FakeClock clock = new FakeClock(LocalDateTime.of(2026, 3, 28, 10, 0));
+
+        Welcome welcome = new Welcome(clock);
+
+        String message = welcome.getMessage();
+
+        assertEquals("Bon week-end", message);
+    }
+}
