047af1f6ffc457d8f403371eb21e8d2887ced260
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-03-27 09:59:48 +0100

    Ajout de tests unitaires

--- src/test/java/WelcomeTest.java
+++ src/test/java/WelcomeTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WelcomeTest {
 
     @Test
-    void shouldReturnBonjourInMorning() {
+    void testsMorningWeek() {
 
-        FakeClock clock = new FakeClock(LocalDateTime.of(2024, 3, 20, 10, 0));
+        FakeClock clock = new FakeClock(LocalDateTime.of(2026, 3, 27, 10, 0));
 
         // Injection du faux temps
         Welcome welcome = new Welcome(clock);
@@ -23,7 +23,25 @@ public class WelcomeTest {
     }
 
     @Test
-    void shouldReturnWeekendMessageOnSunday() {
+    void testsAfternoonWeek() {
+        FakeClock clock = new FakeClock(LocalDateTime.of(2026, 3, 27, 15, 0));
+
+        Welcome welcome = new Welcome(clock);
+        String message = welcome.getMessage();
+        assertEquals("Bon après-midi", message);
+    }
+
+    @Test
+    void testsEveningWeek(){
+        FakeClock clock = new FakeClock(LocalDateTime.of(2026, 3, 25, 21, 0));
+
+        Welcome welcome = new Welcome(clock);
+        String message = welcome.getMessage();
+        assertEquals("Bonsoir", message);
+    }
+
+    @Test
+    void testsWeekend() {
 
         FakeClock clock = new FakeClock(LocalDateTime.of(2026, 3, 28, 10, 0));
 
